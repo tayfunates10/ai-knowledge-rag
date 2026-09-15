9f89facb35deec2c83a6a8b5ef03d67c768deb79
Tayfun <tayfuna766@gmail.com>
2026-09-15 15:02:51 +0300

    feat: add semantic chunk retriever

---
+++ src/retriever.py
@@ -0,0 +1,22 @@
+from vector_math import cosine_similarity
+
+
+def retrieve_best_chunk(query: str, chunks: list[str], model -> tuple[str,float]:
+    query_embedding = model.encode(query).tolist()
+
+    best_chunk = ""
+    best_score = -1,0
+
+    for chunk in chunks:
+        chunk_embedding = model.encode(chunk).tolist()
+
+        score = cosine_similarity(
+            query_embedding,
+            chunk_embedding
+        )
+
+        if score > best_score:
+            best_score = score
+            best_chunk = chunk
+
+      return best_chunk, best_score
